558df5649ad4adaace23fb5cf1225db632574f7b
Mohamed Aitmiloud <mohammed.aitmiloud@gmail.com>
2018-11-22 00:09:53 +0100

    adding logout

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
 
-    <title>AdminLTE 3 | Starter</title>
+    <title>Laravel Blog | Starter</title>
     <link rel="stylesheet" href="{{asset('css/app.css')}}">
     @yield('css')
 </head>
